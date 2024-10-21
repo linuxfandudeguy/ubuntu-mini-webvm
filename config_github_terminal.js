41ac59130028ab01012731f37908bbfab4676a74
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-10-21 16:34:10 -0400

    Update config_github_terminal.js

--- config_github_terminal.js
+++ config_github_terminal.js
@@ -3,7 +3,7 @@ export const diskImageUrl = IMAGE_URL;
 // The root filesystem backend type
 export const diskImageType = "github";
 // Print an introduction message about the technology
-export const printIntro = true;
+export const printIntro = false;
 // Is a graphical display needed
 export const needsDisplay = false;
 // Executable full path (Required)
